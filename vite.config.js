37188a52c97e9dd222be9951940b92079cd5019e
pixidimworld <pixidimworld@users.noreply.github.com>
2026-09-13 18:19:24 -0700

    Configure dev server on port 5175

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
+    port: 5175,
+    strictPort: false,
+    host: "0.0.0.0",
     watch: {
       ignored: ["**/~*.tmp"],
     },
